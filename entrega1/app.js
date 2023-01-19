f921ab2eee45a3ea171689f3fdce4696d82b90a9
Melissa Cecilia Estrada Ramos <98496828+mcer23@users.noreply.github.com>
2023-01-19 14:10:27 -0700

    Add files via upload

---
+++ entrega1/app.js
@@ -0,0 +1,29 @@
+
+function msj(){
+    let input=prompt('¿Que te gustaría hacer?');
+    const prod =['**********','¡Gracias por estar aqui!'];
+    while(input !== 'ESC' && input!=='esc'){
+        if(input==='VER'){
+            for(let i=0;i<prod.length;i++){
+                alert(`${i}: ${prod[i]}`);
+                console.log(`${i}: ${prod[i]}`);
+            }
+        }else if(input==='ADD'){
+            const newProd=prompt('¿Que producto quieres agregar?')
+            prod.push(newProd);
+            alert(`${newProd} se agregó al carrito`)
+        }else if(input==='DEL'){
+            const num=parseInt(prompt('Ingresa el número del producto para eliminarlo'));
+            if(!Number.isNaN(num)){
+                const deleted= prod.splice(num,1)
+                alert(`${deleted[0]} se elimino de tu lista`)
+            }else{
+                alert('Número desconocido')
+            }
+        }
+        input=prompt('¿Que te gustaría hacer?')
+    }
+}
+alert('bye bye')
+msj()
+alert('bye bye')
